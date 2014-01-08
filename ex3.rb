6224c78d4c098428486302503dfaef57fad9cbc5
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-08 19:44:27 +0000

    comments added, explaining what each line does

--- ex3.rb
+++ ex3.rb
@@ -1,9 +1,21 @@
+# declared a variable named "cars" and assigned a value to it
 cars = 100
+
+# declared a variable named "space_in_a_car" and assigned a value to it
 space_in_a_car = 4.0
+
+# declared a variable named "drivers" and assigned a value to it
 drivers = 30
+
+# declared a variable named "passengers" and assigned a value to it
 passengers = 90
+
+# declared a variable named "cars_not_driven" and assigned with (value of "cars" variable) - (value of "drivers" variable)
 cars_not_driven = cars - drivers
+
+# declared a variable named "cars_driven" and assigned with the value of "driver" variable
 cars_driven = drivers
+
 carpool_capacity = cars_driven + space_in_a_car
 average_passenger_per_car = passengers / cars_driven
 
