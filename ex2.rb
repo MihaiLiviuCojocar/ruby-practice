8a8014108efb7c05afbc038a6d94871af320ed18
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-08 19:00:14 +0000

    comments added for every action - ex2.rb

--- ex2.rb
+++ ex2.rb
@@ -1,23 +1,36 @@
+# prints the text inside the curly brackets
 puts "I will now count my chickens: "
 
+# prints "Hens" and the result of 25 + (30 / 6) = 25 + 5 = 30
 puts "Hens", 25 + 30 / 6
+# prints "Roosters" and the result of 100 - (25 * 3 % 4) = 100 - (75 % 4) = 100 - 3 = 97
 puts "Roosters", 100 - 25 * 3 % 4
 
+# prints the text inside the curly brackets
 puts "Now I will count the eggs: "
 
+#prints the result of (3 + 2 + 1 - 5) + (4 % 2) - (1 / 4) + 6 = 1 + 0 - 0 + 6 = 7 
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# prints the text inside the curly brackets
 puts "Is it true that 3 + 2 < 5 - 7 ?"
 
+# prints the result of 5 < -2, which is false
 puts 3 + 2 < 5 - 7
 
+# prints "What is 3 + 2 ?" and, on a new line, prints the result of 3 + 2 = 5
 puts "What is 3 + 2 ?", 3 + 2
+# same thinking like before
 puts "What is 5 - 7 ?", 5 - 7
 
+# prints the text
 puts "Oh, that's why it's false."
-
+# prints the text
 puts "How about some more."
 
+# prints the text and, on a new line, prints the result of 5 > -2, which is true
 puts "Is it greater?", 5 > -2
+# text + true
 puts "Is it greater or equal?", 5 >= -2
+# text + false
 puts "Is it less or equal?", 5 <= -2
