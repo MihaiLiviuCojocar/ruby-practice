dfcf2864d43f8371f85ee8fda7decb77715a7849
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-08 15:41:24 +0000

    added maker file

---
+++ maker.rb
@@ -0,0 +1,7 @@
+# Assign the string "Makers" to a variable and print it out
+people = "Makers";
+puts "The people variable is curently: " + people
+
+# Cange the variable to "Coders" and print it again
+people = "Coders";
+puts "The people variable is curently: " + people
