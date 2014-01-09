2f573220a163d1904bdf93d0d094125fcd3bc709
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-09 09:27:59 +0000

    comments added - ex5.rb

--- ex5.rb
+++ ex5.rb
@@ -1,20 +1,34 @@
+# assign to the var "x" a string
 x = "There are #{10} types of people."
+# assign to the var "binary" a string
 binary = "binary"
+# assign to the var "do_not" a string
 do_not = "don't"
+# asign to the var "y" a string, which contains the value of var "binary" and "do_not"
 y = "Those who know #{binary} and those who #{do_not}."
 
+# print the value of var "x" 
 puts x
+# print the value of var "y"
 puts y
 
+# print a string which contains the value of var "x"
 puts "I said: #{x}."
+# print a string which contains the value of var "y"
 puts "I also said: '#{y}' ."
 
+# assign to the var "hilarious" a boolean value, which is "false"
 hilarious = false
+# assign to the var "joke_evaluation" a string, whici contains the value of var "hilarious"
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# print the value of var "joke_evaluation"
 puts joke_evaluation
 
+# assign to the var "w" a string
 w = "This is the left side of..."
+# assign to the var "e" a string
 e = "a string with a right side."
 
+# print the value of var "w", followed, on the same line, by the value of var "e"
 puts w + e
