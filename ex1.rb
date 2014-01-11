a164b3e06f65910c97d0c9832b1b3cd4037ae6aa
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-11 15:38:29 +0000

    line brake added - ex1.rb

--- ex1.rb
+++ ex1.rb
@@ -1,7 +1,7 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print "Yay! Printing."
+print "Hello World! \n"
+print "Hello Again \n"
+print "I like typing this.\n"
+print "This is fun.\n"
+print "Yay! Printing.\n"
 #puts "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+print 'I "said" do not touch this.\n'
