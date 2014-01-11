26595c01fe1c07903f7c8140a0a2f90e8504bc00
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-11 13:23:22 +0000

    puts changed with print - ex1.rb

--- ex1.rb
+++ ex1.rb
@@ -1,7 +1,7 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts "Yay! Printing."
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
 #puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
+print 'I "said" do not touch this.'
