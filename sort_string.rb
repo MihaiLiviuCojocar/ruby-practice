1752184c71466852c76f24fbd348c702f6e96fee
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-15 16:04:44 +0000

    new sort string() added

--- sort_string.rb
+++ sort_string.rb
@@ -1,10 +1,13 @@
 
 def sort_string(string)
-	arr = []
-	for i in (0..string.length-1)
-		arr << string.slice(i)
-	end
-	print arr.sort.join
+	# arr = []
+	# for i in (0..string.length-1)
+	# 	arr << string.slice(i)
+	# end
+	
+	p string.chars.sort.join
+
+	# print arr.sort.join
 end
 
 sort_string('gergerge')
